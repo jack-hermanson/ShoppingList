41f5fbe875788f774f582f4a4f52ac5d2ba50b02
Jack Hermanson <jack.hermanson@live.com>
2021-02-22 22:15:20 -0700

    get server to run

---
+++ shopping_list/__init__.py
@@ -0,0 +1,25 @@
+import flask_talisman
+from flask_talisman import Talisman
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from shopping_list.config import Config
+
+db = SQLAlchemy()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__,
+                static_url_path='/static',
+                static_folder='web/static',
+                template_folder='web/templates')
+    app.config.from_object(config_class)
+    Talisman(app, content_security_policy=None)
+
+    db.app = app
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    return app
+
