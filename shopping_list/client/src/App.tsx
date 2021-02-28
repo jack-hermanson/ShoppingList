32ed26f5d0bd0e7cf98df41974af150f1ca4506c
Jack Hermanson <jack.hermanson@live.com>
2021-02-28 12:38:44 -0700

    add bootstrap

--- shopping_list/client/src/App.tsx
+++ shopping_list/client/src/App.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
-import './App.css';
 import axios from "axios";
 
 class App extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            response: null
-        };
-    }
-
-    componentDidMount() {
-        axios.get('/api/items/').then(response => {
-            console.log('success');
-            console.log(response);
-            this.setState({response: response});
-        }).catch(error => {
-            console.log(error);
-        })
-    }
 
     render() {
         return (
-            <div className="App">
+            <>
                 <p>Test</p>
-                <p>{this.state.response?.data?.test || "LOADING"}</p>
-            </div>
+            </>
         );
     }
+
+    // constructor(props: any) {
+    //     super(props);
+    //     this.state = {
+    //         response: null
+    //     };
+    // }
+
+    // componentDidMount() {
+    //     axios.get('/api/items/').then(response => {
+    //         console.log('success');
+    //         console.log(response);
+    //         this.setState({response: response});
+    //     }).catch(error => {
+    //         console.log(error);
+    //     })
+    // }
+    //
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <p>Test</p>
+    //             <p>{this.state.response?.data?.test || "LOADING"}</p>
+    //         </div>
+    //     );
+    // }
 }
 
 export default App;
