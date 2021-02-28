0f852f426e670734d6783e326b1384a48ab7cfcf
Jack Hermanson <jack.hermanson@live.com>
2021-02-27 23:08:44 -0700

    get a basic api going

--- shopping_list/__init__.py
+++ shopping_list/__init__.py
@@ -1,7 +1,9 @@
-import flask_talisman
 from flask_talisman import Talisman
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS  # comment out on deployment?
+from .api.HelloApiHandler import HelloApiHandler
 
 from shopping_list.config import Config
 
@@ -10,20 +12,25 @@ db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__,
-                static_url_path='/static',
-                static_folder='web/static',
-                template_folder='web/templates')
+                static_url_path='',
+                static_folder='client/build')
     app.config.from_object(config_class)
     Talisman(app, content_security_policy=None)
-
-    # import and register blueprints
-    from shopping_list.modules.main.routes import main
-    app.register_blueprint(main)
 
     db.app = app
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
+    CORS(app)  # comment out on deployment
+
+    api = Api(app)
+
+    @app.route('/', defaults={'path': ''})
+    def serve(path):
+        return send_from_directory(app.static_folder, 'index.html')
+
+    api.add_resource(HelloApiHandler, '/flask/hello')
 
     return app
 
