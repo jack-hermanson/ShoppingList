dd26d63511f097e0b59b2e81b3b8c6ec09b0114e
Jack Hermanson <jack.hermanson@live.com>
2021-03-27 23:02:45 -0600

    bind new group form to state

--- shopping_list/client/src/components/ShoppingList/Group/NewGroupForm.tsx
+++ shopping_list/client/src/components/ShoppingList/Group/NewGroupForm.tsx
@@ -1,23 +1,42 @@
-import React, {FormEvent} from "react";
+import React, {FormEvent, useState} from "react";
 import {Button, FormGroup, Input, Label} from "reactstrap";
+import GroupModel from "../../../models/GroupModel";
 
 
 export const NewGroupForm = () => {
 
+    const [newGroup, setNewGroup] = useState<GroupModel>({
+        name: "",
+        notes: "",
+        id: null
+    });
+
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log("submitted");
+        console.log({newGroup})
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label required htmlFor="group-name-input">Name</Label>
-                <Input required type="text" placeholder="Name of the category..." id="group-name-input" />
+                <Input required
+                       value={newGroup.name!}
+                       type="text"
+                       placeholder="Name of the category..."
+                       id="group-name-input"
+                       onChange={e => setNewGroup({...newGroup, name: e.target.value})}
+                />
             </FormGroup>
             <FormGroup>
                 <Label htmlFor="group-notes-input">Notes</Label>
-                <Input type="textarea" placeholder="Optional..." id="group-notes-input" />
+                <Input
+                    onChange={e => setNewGroup({...newGroup, notes: e.target.value})}
+                    type="textarea"
+                    placeholder="Optional..."
+                    id="group-notes-input"
+                />
             </FormGroup>
             <FormGroup className="bottom-buttons">
                 <Button block type="submit" color="info">Save</Button>
