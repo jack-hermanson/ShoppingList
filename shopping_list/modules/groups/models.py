c16b113ab841b1fba8955438edce8e9c797575d4
Jack Hermanson <jack.hermanson@live.com>
2021-04-03 21:55:37 -0600

    remove group ids

--- shopping_list/modules/groups/models.py
+++ shopping_list/modules/groups/models.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from shopping_list import db
 
 
@@ -7,12 +5,10 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     notes = db.Column(db.Text)
-    group_items = db.relationship('GroupItem', backref='group', lazy=True)
 
-    def as_dict(self, item_ids: List[int] = None):
+    def as_dict(self):
         return {
             'id': self.id,
             'name': self.name,
-            'notes': self.notes,
-            'itemIds': item_ids if item_ids else []
+            'notes': self.notes
         }
