f04b72e5a9491a5c8d3933d05f598d3d03bb497e
Jack Hermanson <jack.hermanson@live.com>
2021-04-04 10:40:29 -0600

    quick fix for group items

--- shopping_list/modules/groups/models.py
+++ shopping_list/modules/groups/models.py
@@ -5,6 +5,7 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     notes = db.Column(db.Text)
+    group_items = db.relationship('GroupItem', backref='group', lazy=True)
 
     def as_dict(self):
         return {
